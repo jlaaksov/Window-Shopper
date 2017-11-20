185105df42e66bf65597fa094db72ef24a0557a2
Joonas Laaksoviita <laaksoviita.joonas@gmail.com>
2017-11-20 19:50:15 +0200

    custom currency icon

--- window-shopper/Model/Wage.swift
+++ window-shopper/Model/Wage.swift
@@ -11,6 +11,10 @@ import Foundation
 class Wage {
     
     class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        if wage > 0 && price > 0 {
         return Int (ceil(price/wage))
+        } else{
+            return 0
+        }
     }
 }
--- window-shopper/View/CurrencyTextField.swift
+++ window-shopper/View/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5 , y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 0.8027611301)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeTextField()
     }
